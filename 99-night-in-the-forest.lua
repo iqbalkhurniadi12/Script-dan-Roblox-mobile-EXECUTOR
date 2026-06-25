1b1f9008a6b9063d546e40166033eac10d0f4b16
Iqbal craft <rratno280@gmail.com>
2026-06-25 07:51:11 +0700

    Update URL for nightsintheforest.lua script

--- 99-night-in-the-forest.lua
+++ 99-night-in-the-forest.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
+loadstring(game:HttpGet("https://files.vapevoidware.xyz/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wehibuyfgyuwe/99nights.github.io/refs/heads/main/ringta.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameHub.lua"))()
