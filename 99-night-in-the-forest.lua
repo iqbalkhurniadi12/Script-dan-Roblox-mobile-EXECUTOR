2777de0447073c6d8ef97228daea7ab8e913de5d
Iqbal craft <rratno280@gmail.com>
2026-06-28 08:57:09 +0700

    Update script URL for loading FoxnameHub

--- 99-night-in-the-forest.lua
+++ 99-night-in-the-forest.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://files.vapevoidware.xyz/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wehibuyfgyuwe/99nights.github.io/refs/heads/main/ringta.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JNHHGaming/99-nights-in-the-forest/refs/heads/main/H",true))()
